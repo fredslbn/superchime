c808d17e48bd60c02a28bef77ed8df3b270cd542
fredslbn <fh.silaban@gmail.com>
2026-09-17 17:28:38 +0700

    Revert "revert: SYS_faccessat: tweak sdcardfs' handling"
    This reverts commit 4f0a43666d95b65c9c0cbab2e40243d5ff3feeb5.

--- fs/open.c
+++ fs/open.c
@@ -407,7 +407,6 @@ long do_faccessat(int dfd, const char __user *filename, int mode, int flags)
 {
 	struct path path;
 	struct inode *inode;
-	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 	const struct cred *old_cred = NULL;
@@ -435,7 +434,7 @@ retry:
 		goto out;
 
 	inode = d_backing_inode(path.dentry);	
-	mnt = path.mnt;
+	struct vfsmount *mnt = path.mnt;
 
 	if ((mode & MAY_EXEC) && S_ISREG(inode->i_mode)) {
 		/*
